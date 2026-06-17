0bf3eec24ec6aecbf57f6d8361bde020545274f3
MaheshReddy021 <tangiralamaheshreddy70@gmail.com>
2026-06-17 14:48:08 +0530

    Add files via upload

---
+++ 3.PATTERNS/invertrighttriangle.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class invertrighttriangle {
+    public static void main(String args[]){
+        int n=4;
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++)
+
+                System.out.print("* ");
+        
+        System.out.println();
+           
+        } 
+    }
+    
+}
